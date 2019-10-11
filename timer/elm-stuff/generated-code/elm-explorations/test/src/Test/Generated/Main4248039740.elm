2bec18b89d18197be3cf911a372efa0205bd166f
MaiHD <maihd.dev@gmail.com>
2019-10-11 17:04:51 +0700

    timer

---
+++ timer/elm-stuff/generated-code/elm-explorations/test/src/Test/Generated/Main4248039740.elm
@@ -0,0 +1,14 @@
+module Test.Generated.Main4248039740 exposing (main)
+
+import Example
+
+import Test.Reporter.Reporter exposing (Report(..))
+import Console.Text exposing (UseColor(..))
+import Test.Runner.Node
+import Test
+
+main : Test.Runner.Node.TestProgram
+main =
+    [     Test.describe "Example" [Example.suite] ]
+        |> Test.concat
+        |> Test.Runner.Node.run { runs = Nothing, report = (ConsoleReport UseColor), seed = 245341630489704, processes = 12, paths = ["D:\\elm-apps\\timer\\tests\\Example.elm"]}
