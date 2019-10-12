e5c4aa709e69d12b4dc59f2f7126c9e983b93997
MaiHD <maihd.dev@gmail.com>
2019-10-13 01:01:55 +0700

    cat-gifs app

---
+++ cat-gifs/src/Main.elm
@@ -0,0 +1,81 @@
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode exposing (Decoder, field, string)
+
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update 
+        , subscriptions = subscriptions
+        }
+
+type Model
+    = Failure
+    | Loading
+    | Success String
+
+type Msg
+    = MorePlease
+    | GotGif (Result Http.Error String)
+
+init : () -> (Model, Cmd Msg)
+init _ = 
+    (Loading, getRandomCatGif)
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        MorePlease -> (Loading, getRandomCatGif)
+
+        GotGif result ->
+            case result of
+                Ok url ->
+                    (Success url, Cmd.none)
+
+                Err _ -> 
+                    (Failure, Cmd.none)
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h2 [] [ text "Random Cats" ] 
+        , viewGif model
+        ]
+
+viewGif : Model -> Html Msg
+viewGif model =
+    case model of
+        Failure ->
+            div []
+                [ text "I could not load a random cat for some reason." 
+                , button [ onClick MorePlease ] [ text "Try again!" ]
+                ]
+        
+        Loading ->
+            div []
+                [ text "Loading..." ]
+
+        Success url ->
+            div []
+                [ button [ onClick MorePlease, style "display" "block" ] [ text "More please!" ] 
+                , img [ src url ] []
+                ]
+
+getRandomCatGif : Cmd Msg
+getRandomCatGif = 
+    Http.get
+        { url = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cat"
+        , expect = Http.expectJson GotGif gifDecoder
+        }
+
+gifDecoder : Decoder String
+gifDecoder = 
+    field "data" (field "image_url" string)
